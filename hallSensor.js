1a2c2cf2cd3393836ca6d15ec066b40a3e6b774d
geoffreypetri <g.petri@me.com>
2017-05-08 17:16:54 +0200

    Add hallSensor

---
+++ hallSensor.js
@@ -0,0 +1,39 @@
+/**
+ * Hall sensor
+ *
+ */
+
+const config     = require('./config')
+  , sensors      = config.sensors
+  , defaultPin   = config.defaultPin
+  , debug        = require('debug')('wfs:HallSensor')
+  , GPIO         = require('onoff').Gpio
+
+module.exports = class HallSensor {
+
+  constructor(pin = defaultPin) {
+
+    // Init sensor
+    this._i             = 0
+    this._isRunning     = false
+
+    // Sensor characteristics
+    this._pin           = pin
+    this._sensor        = new GPIO(pin, 'in', 'rising')
+
+    // Watch ticks to increment counter
+    this._sensor.watch(this.increment.bind(this))
+
+    debug(`Hall sensor on pin ${this._pin} initialized`)
+  }
+
+  get count() {
+    return this._i
+  }
+
+  increment(err, state) {
+    this._i++
+
+    debug(`Hall sensor on pin ${this._pin} count ${this._i}`)
+  }
+}
