3d86c0e0ce3ffc678cf4f5e7a71e0655f33fdfae
geoffreypetri <g.petri@me.com>
2017-05-09 21:54:35 +0200

    Test YF-S201C sensor

--- config.js
+++ config.js
@@ -5,37 +5,20 @@ const config = {}
 config.sensors = []
 
 config.sensors['POW110D3B'] = {}
-config.sensors['POW110D3B'].countToFlow   = 1 / 7.5 // Pulse frequency (Hz) in Horizontal Test= 7.5Q, Q is flow rate in L/min. (Results in +/- 3% range)
-config.sensors['POW110D3B'].countToVolume = 0.002217 //
-config.sensors['POW110D3B'].factor        = 7.51766 //
+// F (Hz) = 7.5 * Q (L/min)
+config.sensors['POW110D3B'].factor  = 7.51766
 
 config.sensors['YF-S201'] = config.sensors['POW110D3B']
 
 config.sensors['YF-S201C'] = {}
-// Q (L/m) = F (Hz) / 5 (± 3%)
-config.sensors['YF-S201C'].countToFlow    = 1 / 6.29
-// TODO Q (L/min) / 60
-config.sensors['YF-S201C'].countToVolume  = 0,0026497
+// F (Hz) = 5 * Q (L/min)
+config.sensors['POW110D3B'].factor  = 6.29
 
 /* Default pin and model */
 
 config.defaultPin   = 17
 config.defaultModel = 'POW110D3B'
 config.defaultDelay = 1000
-
-/**
- * Database configuration
- */
-
-config.db = {}
-config.db.server = 'mongo'
-config.db.protocol = 'mongodb'
-config.db.host = 'localhost'
-config.db.port = '27017'
-config.db.name = 'water'
-config.db.url = config.db.protocol + '://' + config.db.host + ':'
-                  + config.db.port + '/' + config.db.name
-
 
 /* Exports */
 
--- test/run.js
+++ test/run.js
@@ -1,5 +1,6 @@
 var wfs     = require('../index')
-  , sensor  = new wfs(17, 'POW110D3B', logger)
+  //, sensor  = new wfs(17, 'POW110D3B', logger)
+  , sensor  = new wfs(27, 'YF-S201C', logger)
   , total   = 0
 
 //sensor.delay = 1000
