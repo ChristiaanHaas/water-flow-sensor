717f4eda9424ef079db77394a838b5cfe0bea59e
geoffreypetri <g.petri@me.com>
2017-05-10 10:07:42 +0200

    Remove total

--- test/run.js
+++ test/run.js
@@ -1,7 +1,6 @@
 var wfs     = require('../index')
   , sensor  = new wfs(17, 'POW110D3B', logger)
   , sensor2 = new wfs(27, 'YF-S201C', logger)
-  , total   = 0
 
 function logger(res) {
   console.info(res)
